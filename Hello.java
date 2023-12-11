380b4ffccbf1cb8840fdf99407caf217be9049c0
Rahul <rahul.1rn17ec117@gmail.com>
2023-12-11 22:17:51 +0530

    #2 Added tables

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
 class hello {
+
+  public void tables(int number) {
+    for (int i = 1; i < 11; i++) {
+      System.out.println(number + "*" + i + "=" + number * i);
+    }
+
+  }
   public static void main(String[] args) {
     System.out.println("Hello world");
+    hello table = new hello();
+    table.tables(14);
   }
+
 }
